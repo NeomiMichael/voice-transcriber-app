69be506a08a3ee0dab2af34db2dc6cdeed4656d5
MichalFog <mf058326035@gmail.com>
2025-07-21 23:16:51 +0300

    fix: remove submodule and add transcription-client as regular folder

---
+++ transcription-client/src/features/Dashboard/pages/DashboardPage.tsx
@@ -0,0 +1,16 @@
+import UploadAudioForm from '../components/UploadAudioForm'
+
+function DashboardPage() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1> 🎧ברוכה הבאה למערכת התמלול המקצועית</h1>
+        <p>כאן תוכלי להעלות קובצי שמע ולקבל תמלול</p>
+        <p>feere</p>
+        <UploadAudioForm />
+      </div>
+    )
+  }
+  
+  
+  export default DashboardPage
+  
