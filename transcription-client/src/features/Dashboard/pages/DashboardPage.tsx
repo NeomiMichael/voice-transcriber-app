df26bd7e5d08b89c321ebe5a3102ed0457980e87
MichalFog <mf058326035@gmail.com>
2025-07-21 23:19:19 +0300

    fix:

--- transcription-client/src/features/Dashboard/pages/DashboardPage.tsx
+++ transcription-client/src/features/Dashboard/pages/DashboardPage.tsx
@@ -5,7 +5,6 @@ function DashboardPage() {
       <div style={{ textAlign: 'center', marginTop: '50px' }}>
         <h1> 🎧ברוכה הבאה למערכת התמלול המקצועית</h1>
         <p>כאן תוכלי להעלות קובצי שמע ולקבל תמלול</p>
-        <p>feere</p>
         <UploadAudioForm />
       </div>
     )
